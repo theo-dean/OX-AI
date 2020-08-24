3ca565fe0b8b776d68b5fa60bfc0f6255c07bd76
Theo <theodeancsee@gmail.com>
2020-08-24 16:08:47 +0100

    Updated Board class

--- ox-ai-backend-server/src/Board.java
+++ ox-ai-backend-server/src/Board.java
@@ -1,18 +1,39 @@
 public class Board {
     int[] states;   //Array with integer representation for states of each cell on the board.
-                        //Where: 0 - empty, 1 - naught, 2 - cross.
+    //Where: 0 - empty, 1 - naught, 2 - cross.
 
-    public Board(){
+    public Board() {
         this.states = new int[9];
     }
 
-    int[] getStates(){
+    int[] getStates() {
         return this.states;
     }
 
-    int getStates(int pos){
+    int getStates(int pos) {
         return this.states[pos];
     }
 
-    public String
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println(board.toString());
+    }
+
+    public String toString() {
+        char[] grid = new char[9];
+        for (int i = 0; i < states.length; i++) {
+            switch (states[i]) {
+                case (0):
+                    grid[i] = '-';
+                    break;
+                case (1):
+                    grid[i] = 'O';
+                    break;
+                case (2):
+                    grid[i] = 'X';
+                    break;
+            }
+        }
+        return(String.format("%c %c %c\n%c %c %c\n%c %c %c", grid[0],grid[1],grid[2],grid[3],grid[4],grid[5],grid[6],grid[7],grid[8]));
+    }
 }
