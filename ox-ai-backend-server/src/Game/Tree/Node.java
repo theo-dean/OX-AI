6267cc5905f7d026b6bf2e6059473994edd29161
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2020-09-30 18:49:01 +0100

    Starter working on the game tree. TODO: Add methods for traversing the tree
    TODO: Finish Game Tree, start MCTS

--- ox-ai-backend-server/src/Game/Tree/Node.java
+++ ox-ai-backend-server/src/Game/Tree/Node.java
@@ -5,8 +5,35 @@ import Game.Board;
 import java.util.ArrayList;
 
 public class Node {
-    private Board root;
-    private ArrayList<Board> children;
+    private Board data;
+    private Node root;
+    private ArrayList<Node> children;
 
+    Node(Board data){       //Root node constructor
+        this.data = data;
+        this.root = null;
+    }
 
+    Node(Board data, Node root){    //Branch and leaf constructor
+        this.data = data;
+        this.root = root;
+    }
+
+    void addChild(Board data){
+        if (children == null){
+            children = new ArrayList<>();   //Maybe should initialise in constructor? memory vs computations
+        }
+        children.add(new Node(data, this));
+    }
+
+    boolean isLeaf(){
+        if (children == null){
+            return true;
+        }
+        else {return false;}        //Could also check if children is empty
+    }
+
+    Board getData(){
+        return this.data;
+    }
 }
---
+++ ox-ai-backend-server/src/Game/Tree/Tree.java
@@ -0,0 +1,7 @@
+package Game.Tree;
+
+public class Tree {
+
+
+
+}
