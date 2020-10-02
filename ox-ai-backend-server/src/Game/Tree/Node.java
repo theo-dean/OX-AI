4e4bda987089935f2026cb984bffaf4f99659754
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2020-10-03 00:36:41 +0100

    Worked more on game state tree TODO: Add methods for in-order traversing the tree
    TODO: Finish Game Tree, start MCTS

--- ox-ai-backend-server/src/Game/Tree/Node.java
+++ ox-ai-backend-server/src/Game/Tree/Node.java
@@ -1,13 +1,12 @@
 package Game.Tree;
 
 import Game.Board;
-
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Node {
     private Board data;
     private Node root;
-    private ArrayList<Node> children;
+    private LinkedList<Node> children;
 
     Node(Board data){       //Root node constructor
         this.data = data;
@@ -21,16 +20,17 @@ public class Node {
 
     void addChild(Board data){
         if (children == null){
-            children = new ArrayList<>();   //Maybe should initialise in constructor? memory vs computations
+            children = new LinkedList<>();   //Maybe should initialise in constructor? memory vs computations
         }
         children.add(new Node(data, this));
     }
 
     boolean isLeaf(){
-        if (children == null){
-            return true;
-        }
-        else {return false;}        //Could also check if children is empty
+        return children == null;        //Could also check if children is empty
+    }
+
+    boolean isRoot(){
+        return root == null;
     }
 
     Board getData(){
--- ox-ai-backend-server/src/Game/Tree/Tree.java
+++ ox-ai-backend-server/src/Game/Tree/Tree.java
@@ -1,7 +1,19 @@
 package Game.Tree;
 
+import java.util.LinkedList;
+
 public class Tree {
 
+    void printInOrderTree(Node node){
+
+    }
 
 
+    public static void main(String[] args){
+        LinkedList<String> layer = new LinkedList<>();
+        layer.add("first");
+        layer.add("second");
+        layer.add("third");
+
+    }
 }
